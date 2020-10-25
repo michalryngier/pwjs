0305ca723a32b89afbf9004b8d251f19ad6a735c
Michal Ryngier <michal.ryngier@gmail.com>
2020-10-25 21:59:19 +0100

    fix: if statement

--- student-scripts/problem005.js
+++ student-scripts/problem005.js
@@ -1,4 +1,4 @@
 function strangeSum(a, b) {
-  if (isNaN(a) || isNaN(b) || Number.isInteger(a) === false || Number.isInteger(b) === false) return null;
+  if (Number.isInteger(a) === false || Number.isInteger(b) === false) return null;
   return a === b ?  3 * (a + b) :  a + b;
 }
