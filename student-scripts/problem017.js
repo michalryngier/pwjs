6658fad8e398e4b5d87cfbe6484401159876ec3b
m.ryngier <michal.ryngier@gmail.com>
2020-11-16 11:25:33 +0100

    feat: problem 17 not solved

--- student-scripts/problem017.js
+++ student-scripts/problem017.js
@@ -1,13 +1,4 @@
 function compose(a) {
-  let func = x => x;
-  if (arguments.length > 0) {
-    for (let i = arguments.length - 1; i <= 0 ; --i) {
-      if (i === arguments.length - 1) {
-        func = x => arguments[i];
-      } else {
-        func = func(arguments[i]);
-      }
-    }
-  }
-  return func;
+  //dunno
+  return a;
 }
